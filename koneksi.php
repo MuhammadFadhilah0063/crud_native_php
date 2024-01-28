b0e91ad31efd8d7fa70559ce4d38ced7d6a4aaff
MuhammadFadhilah0063 <uwaiesm@gmail.com>
2024-01-28 12:26:07 +0800

    ubah konfigurasi koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-  $koneksi = mysqli_connect("localhost", "dhilah", "F4dhil01", "db_test") or die("Gagal terhubung ke basisdata: " . mysqli_error($koneksi));
+  $koneksi = mysqli_connect("localhost", "u708144562_dhilah", "F4dhil01", "u708144562_db_test") or die("Gagal terhubung ke basisdata: " . mysqli_error($koneksi));
 } catch (Exception $e) {
   echo "Gagal terhubung: " . $e->getMessage();
 }
